b3cae366afea94e5c3ed9b102fe8bf34c257128d
Mahak Aggarwal <39831824+MahakAggarwal@users.noreply.github.com>
2018-10-25 20:49:12 +0400

    Add files via upload

---
+++ 1.2_Part_A_Your_first_interactive_UI/HelloToast/app/src/main/java/me/mahakagg/hellotoast/MainActivity.java
@@ -0,0 +1,35 @@
+package me.mahakagg.hellotoast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+//        code for button toast goes here
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+//        code for button count goes here
+        mCount ++;
+        if (mShowCount != null){
+            mShowCount.setText(Integer.toString(mCount));
+        }
+    }
+}

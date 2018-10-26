676cd66fa0dd6ce34fbf941f4810dcbe16d1f927
Mahak Aggarwal <39831824+MahakAggarwal@users.noreply.github.com>
2018-10-26 12:35:15 +0400

    Add files via upload

---
+++ 2.2_Activity_lifecycle_and_state/ShoppingList/app/src/main/java/me/mahakagg/shoppinglist/MainActivity.java
@@ -0,0 +1,83 @@
+package me.mahakagg.shoppinglist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+    public static final int ITEM_REQUEST = 1;
+    private final TextView[] item = new TextView[10];
+    private ArrayList<String> itemsList = new ArrayList<>(10);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        item[0] = findViewById(R.id.item1);
+        item[1] = findViewById(R.id.item2);
+        item[2] = findViewById(R.id.item3);
+        item[3] = findViewById(R.id.item4);
+        item[4] = findViewById(R.id.item5);
+        item[5] = findViewById(R.id.item6);
+        item[6] = findViewById(R.id.item7);
+        item[7] = findViewById(R.id.item8);
+        item[8] = findViewById(R.id.item9);
+        item[9] = findViewById(R.id.item10);
+
+        if (savedInstanceState != null) {
+            itemsList = savedInstanceState.getStringArrayList("ItemsListStringArray");
+            int i;
+            if (itemsList != null && itemsList.size() > 0) {
+                for (i = 0; i < itemsList.size(); i++) {
+                    if (itemsList.size() > 10) {
+                        Toast.makeText(MainActivity.this, "Cannot add more items to the list", Toast.LENGTH_LONG).show();
+                        break;
+                    }
+                    item[i].setVisibility(View.VISIBLE);
+                    item[i].setText(itemsList.get(i));
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (itemsList.size() != 0) {
+            outState.putStringArrayList("ItemsListStringArray", itemsList);
+        }
+    }
+
+    public void addItem(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivityForResult(intent, ITEM_REQUEST);
+    }
+
+    // method for when SecondActivity returns intent
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ITEM_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String itemString = data.getStringExtra(SecondActivity.EXTRA_ITEMS);
+                itemsList.add(itemString);
+                int i;
+                for (i = 0; i < itemsList.size(); i++) {
+                    if (itemsList.size() > 10) {
+                        Toast.makeText(MainActivity.this, "Cannot add more items to the list", Toast.LENGTH_LONG).show();
+                        break;
+                    }
+                    item[i].setVisibility(View.VISIBLE);
+                    item[i].setText(itemsList.get(i));
+                }
+
+            }
+        }
+    }
+}
---
+++ 2.2_Activity_lifecycle_and_state/ShoppingList/app/src/main/java/me/mahakagg/shoppinglist/SecondActivity.java
@@ -0,0 +1,59 @@
+package me.mahakagg.shoppinglist;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+    public static final String EXTRA_ITEMS = "me.mahakagg.shoppinglist.extra.ITEMS";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+//        Intent intent = getIntent();
+    }
+
+    public void selectItem(View view) {
+        Intent return_items_intent = new Intent();
+
+        switch (view.getId()){
+            case R.id.button_Chocolate:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Chocolate"); setResult(RESULT_OK, return_items_intent); finish();
+                break;
+            case R.id.button_oil:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Oil"); setResult(RESULT_OK, return_items_intent); finish();
+                break;
+            case R.id.button_milk:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Milk"); setResult(RESULT_OK, return_items_intent); finish();
+                break;
+            case R.id.button_eggs:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Eggs"); setResult(RESULT_OK, return_items_intent); finish();
+                break;
+            case R.id.button_cheese:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Cheese"); setResult(RESULT_OK, return_items_intent); finish();
+                break;
+            case R.id.button_rice:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Rice"); setResult(RESULT_OK, return_items_intent); finish();
+                break;
+            case R.id.button_wheat:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Wheat Flour"); setResult(RESULT_OK, return_items_intent); finish();
+                break;
+            case R.id.button_bread:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Bread"); setResult(RESULT_OK, return_items_intent); finish();
+                break;
+            case R.id.button_sugar:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Sugar"); setResult(RESULT_OK, return_items_intent); finish();
+                break;
+            case R.id.button_salt:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Salt"); setResult(RESULT_OK, return_items_intent); finish();
+                break;
+            default:
+                Toast.makeText(SecondActivity.this, "There was an error", Toast.LENGTH_LONG).show();
+                break;
+        }
+
+    }
+}

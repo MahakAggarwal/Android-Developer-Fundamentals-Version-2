99f35943367ef159dbf3e08aba9f9bae112a0dce
Mahak Aggarwal <39831824+MahakAggarwal@users.noreply.github.com>
2019-03-20 18:37:16 +0400

    Add files via upload

---
+++ 7.2_AsyncTask_and_AsyncTaskLoader/WhoWroteItLoaderCC/app/src/main/java/me/mahakagg/whowroteitloadercc/BookLoader.java
@@ -0,0 +1,28 @@
+package me.mahakagg.whowroteitloadercc;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.AsyncTaskLoader;
+
+public class BookLoader extends AsyncTaskLoader<String> {
+
+    private String mQueryString;
+
+    public BookLoader(@NonNull Context context, String queryString) {
+        super(context);
+        mQueryString = queryString;
+    }
+
+    @Nullable
+    @Override
+    public String loadInBackground() {
+        return NetworkUtils.getBookInfo(mQueryString);
+    }
+
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+        forceLoad();
+    }
+}
